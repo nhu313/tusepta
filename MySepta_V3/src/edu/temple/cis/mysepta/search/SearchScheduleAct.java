c6ca6a4cc6b41a0ff988e21d6b908d93d7addb9a
tub75485@temple.edu <tub75485@temple.edu@917f48b0-186b-11df-8d17-3de44cf3a393>
2010-04-30 17:54:24 +0000

    Wan

--- MySepta_V3/src/edu/temple/cis/mysepta/search/SearchScheduleAct.java
+++ MySepta_V3/src/edu/temple/cis/mysepta/search/SearchScheduleAct.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import edu.temple.cis.mysepta.R;
 import edu.temple.cis.mysepta.data.DBAdapter;
 import edu.temple.cis.mysepta.data.MySeptaDataHelper;
@@ -22,6 +23,8 @@ public class SearchScheduleAct extends MySeptaScreen {
 	private ListView lv;
 	private ArrayAdapter<Schedule> aa;
 	List<Schedule> list;
+	private static final String[] am_pm = {"AM", "PM"};  
+	Spinner spinner1; 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +38,9 @@ public class SearchScheduleAct extends MySeptaScreen {
 		
 		stopId = getIntent().getExtras().getLong(DBAdapter.KEY_StopNameID);
 		list = db.getSchedule(stopId, MySeptaDataHelper.getTime());
-		
+		spinner1 = (Spinner)findViewById(R.id.Spinner01);  
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, am_pm);  
+		spinner1.setAdapter(adapter);
 		aa = new ArrayAdapter<Schedule>(this, R.layout.list_row_large, list);
 		lv = (ListView)findViewById(R.id.search_list);
 		lv.setAdapter(aa);
@@ -48,7 +53,11 @@ public class SearchScheduleAct extends MySeptaScreen {
 		@Override
 		public void onClick(View v) {
 			EditText txttimesearch=(EditText)findViewById(R.id.search_sched_txttime);
-			time = Float.parseFloat(txttimesearch.getText().toString());			
+			String s = (String)spinner1.getSelectedItem();
+			time = Float.parseFloat(txttimesearch.getText().toString());
+			if(s == "PM"){
+				time = time + 12;
+			}
 			handleSearch();
 		}
 	}
